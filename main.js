64bf1b2790f9a3dc1556bb1e1fdc7bf31423ac0f
Hassan Mostafa <hassan.bmw2251@users.noreply.github.com>
2020-08-12 01:35:26 +0200

    NextSlotAdded

--- main.js
+++ main.js
@@ -13,21 +13,22 @@
 
 
 
- function myFunc1() {
-     h11.innerHTML = input1.value
- }
+ //  function myFunc1() {
 
- function myFunc2() {
-     h12.innerHTML = input2.value
- }
+ //  }
 
- function myFunc3() {
-     h13.innerHTML = input3.value
- }
+ //  function myFunc2() {
 
- function myFunc4() {
-     h14.innerHTML = input4.value
- }
+
+ //  }
+
+ //  function myFunc3() {
+
+ //  }
+
+ //  function myFunc4() {
+ //      h14.innerHTML = input4.value;
+ //  }
 
  function enterYear() {
      //  if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);
@@ -44,10 +45,10 @@
  var submitBTN = document.querySelector('.submitBTN');
  var enterBTN = document.querySelector(".enterBTN");
  submitBTN.addEventListener('click', () => {
-
-     validation();
-     dateValidation();
-
+     if (validation() === true && dateValidation() === true)
+         //  validation();
+         //  dateValidation();
+         alert("SENT")
 
  })
 
@@ -58,11 +59,52 @@
      }
      if (isNaN(input1.value) || isNaN(input2.value) || isNaN(input3.value) || isNaN(input4.value)) {
          alert("CC Number Must be Integer Numbers");
+     } else {
+         return (true)
      }
  }
 
  function dateValidation() {
      if (year.value.length > 2 || month.value.length > 2 || month.value > 12 || year.value > 20 || year.value == "" || month.value == "") {
          alert("Please Fill In the right Date Info")
+     } else {
+         return (true)
      }
+ }
+
+ //---------------------------------------Next SLot-----------
+
+ input1.oninput = function () {
+     h11.textContent = input1.value;
+     if (input1.value.length == 4) {
+         input2.focus()
+     } else {
+         console.log("works");
+     }
+ };
+
+ input2.oninput = function () {
+     h12.innerHTML = input2.value;
+     if (input2.value.length == 4) {
+         input3.focus();
+     };
+ };
+
+ input3.oninput = function () {
+     h13.innerHTML = input3.value;
+     if (input3.value.length == 4) {
+         input4.focus();
+     };
+ };
+ input4.oninput = function () {
+     h14.innerHTML = input4.value;
+     if (input4.value.length == 4) {
+         month.focus();
+     };
+ };
+ month.oninput = function () {
+     dateM.innerHTML = month.value;
+     if (month.value.length == 2) {
+         year.focus();
+     };
  }
