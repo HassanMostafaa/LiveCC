4a1d77aa50c09f4eef6d72c4c7c6476ff7b32cf0
Hassan Mostafa <hassan.bmw2251@users.noreply.github.com>
2020-08-12 02:01:09 +0200

    If statement Fixed

--- main.js
+++ main.js
@@ -11,66 +11,49 @@
  var month = document.getElementById('month'); //da el input
  var year = document.getElementById('year'); //da el input
 
-
-
- //  function myFunc1() {
-
- //  }
-
- //  function myFunc2() {
-
-
- //  }
-
- //  function myFunc3() {
-
- //  }
-
- //  function myFunc4() {
- //      h14.innerHTML = input4.value;
- //  }
-
  function enterYear() {
-     //  if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);
      dateY.innerHTML = year.value;
-
  }
 
  function enterMonth() {
-     //  if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);
      dateM.innerHTML = month.value;
-
  }
-
- var submitBTN = document.querySelector('.submitBTN');
- var enterBTN = document.querySelector(".enterBTN");
- submitBTN.addEventListener('click', () => {
-     if (validation() === true && dateValidation() === true)
-         //  validation();
-         //  dateValidation();
-         alert("SENT")
-
- })
-
 
  function validation() {
      if (input1.value == "" || input2.value == "" || input3.value == "" || input4.value == "" || input1.value.length < 4 || input2.value.length < 4 || input3.value.length < 4 || input4.value.length < 4) {
          alert("Please Fill in all The Forms");
+         return false;
      }
      if (isNaN(input1.value) || isNaN(input2.value) || isNaN(input3.value) || isNaN(input4.value)) {
          alert("CC Number Must be Integer Numbers");
+         return false;
      } else {
-         return (true)
+         return true;
      }
  }
 
  function dateValidation() {
      if (year.value.length > 2 || month.value.length > 2 || month.value > 12 || year.value > 20 || year.value == "" || month.value == "") {
-         alert("Please Fill In the right Date Info")
+         alert("Please Fill In the right Date Info");
+         return false;
      } else {
-         return (true)
+         return true;
      }
  }
+
+ var submitBTN = document.querySelector('.submitBTN');
+ var enterBTN = document.querySelector(".enterBTN");
+ submitBTN.addEventListener('click', () => {
+     if (validation() && dateValidation()) {
+         //  validation();
+         //  dateValidation();
+         alert("SENT")
+     } else {
+         console.error("Please Check Your Input Informations Before submitting!");
+     }
+
+
+ })
 
  //---------------------------------------Next SLot-----------
 
@@ -78,8 +61,6 @@
      h11.textContent = input1.value;
      if (input1.value.length == 4) {
          input2.focus()
-     } else {
-         console.log("works");
      }
  };
 
@@ -106,5 +87,11 @@
      dateM.innerHTML = month.value;
      if (month.value.length == 2) {
          year.focus();
+     };
+ }
+ year.oninput = function () {
+     dateY.innerHTML = year.value;
+     if (year.value.length == 2) {
+         submitBTN.focus();
      };
  }
